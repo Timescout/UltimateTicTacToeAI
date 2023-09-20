0b8fc05dd2e36ee252f354839c9422fd7c654530
Andrew Bergman <timepolicescout@gmail.com>
2023-09-20 12:19:11 -0700

    added some tests

--- code/tests/MoveTests.cpp
+++ code/tests/MoveTests.cpp
@@ -1,5 +1,47 @@
 /* Andrew Bergman
 9-14-23
-
-
+Tests for the Move struct
 */
+
+#include "State.h"
+#include <gtest/gtest.h>
+
+TEST(MoveTests, ToBinary_Creates_CorrectBinary)
+{
+    move m(activeBoard::board1, 1);
+
+    int binary = m.toBinary();
+
+    EXPECT_EQ(binary, 0b00010001);
+}
+
+TEST(MoveTests, FromBinary_Creates_CorrectBinary)
+{
+    int binary = 0b00010001; //represents a move on board 1 space 1
+
+    move m(binary);
+
+    EXPECT_EQ(m.board, activeBoard::board1);
+    EXPECT_EQ(m.space, 1);
+}
+
+TEST(MoveTests, FromBinary_InvalidBoard_ThrowsError)
+{
+    int binary = 0b11010000;
+
+    EXPECT_THROW(move m(binary), std::out_of_range);
+}
+
+TEST(MoveTests, FromBinary_InvalidSpace_ThrowsError)
+{
+    int binary = 0b00001111;
+
+    EXPECT_THROW(move m(binary), std::out_of_range);
+}
+
+TEST(MoveTests, FromBinary_AnyBoardValue_ThrowsError)
+{
+    int binary = 0b11110000;
+
+    EXPECT_THROW(move m(binary), std::out_of_range);
+}
--- code/tests/Ultimate3TStateTests.cpp
+++ code/tests/Ultimate3TStateTests.cpp
@@ -8,3 +8,10 @@ Tests for the Ultimate3TState class.
 
 // TEST(Ultimate3TTests, ) 
 // {}
+
+TEST(Ultimate3TState, setSpacePlayed_invalidSpace_ThrowsError)
+{
+    Ultimate3TState state;
+
+    EXPECT_THROW(state.setSpacePlayed(0, 9, player::x), std::out_of_range);
+}
