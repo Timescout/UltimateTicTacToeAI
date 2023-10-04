ffb035e1b01f8096658c2c6c096c17d942b0b381
Andrew Bergman <timepolicescout@gmail.com>
2023-10-04 13:08:19 -0700

    refactored for readability

--- code/tests/evaluationValueTests.cpp
+++ code/tests/evaluationValueTests.cpp
@@ -14,11 +14,31 @@ class EvaluationUnitTestTrueResult :
 // Test cases for The following Test
 INSTANTIATE_TEST_SUITE_P(VariousTrueGreaterThanTests, EvaluationUnitTestTrueResult, testing::Values
 (
-    std::vector<evaluationValue>({evaluationValue(player::x, 0), evaluationValue(player::o, 0)}),
-    std::vector<evaluationValue>({evaluationValue(player::x, 81), evaluationValue(player::draw, 81)}),
-    std::vector<evaluationValue>({evaluationValue(player::draw, 0), evaluationValue(player::o, 0)}),
-    std::vector<evaluationValue>({evaluationValue(player::draw, 0), evaluationValue(player::o, 0)}),
-    std::vector<evaluationValue>({evaluationValue(player::o, 1), evaluationValue(player::o, 0)})
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::x, 0),
+        evaluationValue(player::o, 0)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::x, 81), 
+        evaluationValue(player::draw, 81)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::draw, 0),
+        evaluationValue(player::o, 0)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::draw, 0),
+        evaluationValue(player::o, 0)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::o, 1),
+        evaluationValue(player::o, 0)
+    })
 ));
 TEST_P(EvaluationUnitTestTrueResult, GreaterThan_FirstGreaterThanSecond_Returns_True)
 {
@@ -36,10 +56,26 @@ class EvaluationUnitTestFalseResult :
 // Test cases for The following Test
 INSTANTIATE_TEST_SUITE_P(VariousFalseGreaterThanTests, EvaluationUnitTestFalseResult, testing::Values
 (
-    std::vector<evaluationValue>({evaluationValue(player::draw, 0), evaluationValue(player::draw, 0)}),
-    std::vector<evaluationValue>({evaluationValue(player::o, 81), evaluationValue(player::draw, 81)}),
-    std::vector<evaluationValue>({evaluationValue(player::draw, 81), evaluationValue(player::x, 81)}),
-    std::vector<evaluationValue>({evaluationValue(player::o, 81), evaluationValue(player::x, 81)})
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::draw, 0), 
+        evaluationValue(player::draw, 0)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::o, 81), 
+        evaluationValue(player::draw, 81)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::draw, 81),
+        evaluationValue(player::x, 81)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::o, 81), 
+        evaluationValue(player::x, 81)
+    })
 ));
 TEST_P(EvaluationUnitTestFalseResult, GreaterThan_FirstLessThanOrEqualTo_Returns_False)
 {
@@ -57,17 +93,61 @@ class EvaluationUnitTestThrowResult :
 // Test cases for The following Test
 INSTANTIATE_TEST_SUITE_P(VariousInvalidGreaterThanTests, EvaluationUnitTestThrowResult, testing::Values
 (
-    std::vector<evaluationValue>({evaluationValue(player::neither, 0), (evaluationValue(player::x, 0))}),
-    std::vector<evaluationValue>({evaluationValue(player::x, 0), (evaluationValue(player::neither, 0))}),
-    std::vector<evaluationValue>({evaluationValue(player::x, 82), (evaluationValue(player::x, 0))}),
-    std::vector<evaluationValue>({evaluationValue(player::x, -1), (evaluationValue(player::x, 0))}),
-    std::vector<evaluationValue>({evaluationValue(player::x, 0), (evaluationValue(player::x, 82))}),
-    std::vector<evaluationValue>({evaluationValue(player::x, 0), (evaluationValue(player::x, -1))}),
-    std::vector<evaluationValue>({evaluationValue(player::neither, 0), (evaluationValue(player::neither, 0))}),
-    std::vector<evaluationValue>({evaluationValue(player::neither, 0), (evaluationValue(player::draw, 0))}),
-    std::vector<evaluationValue>({evaluationValue(player::neither, 0), (evaluationValue(player::o, 0))}),
-    std::vector<evaluationValue>({evaluationValue(player::o, 0), (evaluationValue(player::neither, 0))}),
-    std::vector<evaluationValue>({evaluationValue(player::draw, 0), (evaluationValue(player::neither, 0))})
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::neither, 0), 
+        evaluationValue(player::x, 0)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::x, 0), 
+        evaluationValue(player::neither, 0)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::x, 82), 
+        evaluationValue(player::x, 0)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::x, -1), 
+        evaluationValue(player::x, 0)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::x, 0), 
+        evaluationValue(player::x, 82)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::x, 0),
+        evaluationValue(player::x, -1)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::neither, 0), 
+        evaluationValue(player::neither, 0)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::neither, 0), 
+        evaluationValue(player::draw, 0)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::neither, 0),
+        evaluationValue(player::o, 0)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::o, 0), 
+        evaluationValue(player::neither, 0)
+    }),
+    std::vector<evaluationValue>
+    ({
+        evaluationValue(player::draw, 0),
+        evaluationValue(player::neither, 0)
+    })
 ));
 TEST_P(EvaluationUnitTestThrowResult, GreaterThan_EitherInvalid_Throws_InvalidArgument)
 {
