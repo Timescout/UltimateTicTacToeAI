004127b11e8aff3a43f15af44eaa7c4d0613c7b9
Andrew Bergman <timepolicescout@gmail.com>
2023-10-13 15:36:26 -0700

    added AgentTrainer class, EncodingCompare struct

--- code/headers/Agent.h
+++ code/headers/Agent.h
@@ -3,39 +3,38 @@ Ultimate Tic Tac Toe AI project
 Andrew Bergman
 9/11/23
 
-This file will contain the game code of the AI Agent who plays the game.
+This file will contain the game code of the AI Agent who plays the game and the trainer class that creates the move file the the AI will read from.
 
 */
+#pragma once
 #include "State.h"
-#include <vector>
+#include <set>
 
-class Agent
+/// @brief Used to compare two bitsets of ENCODINGSIZE bits. This function is used to create a set of encodings.
+struct EncodingCompare
 {
-
+    /// @brief Equivilant to a < b
+    /// @param a First bitset to compare.
+    /// @param b Second bitset to compare.
+    bool operator()(std::bitset<ENCODINGSIZE> a, std::bitset<ENCODINGSIZE> b) const;
 };
 
-// evaluation study(Ultimate3TState gameState)
-// {
-//     if (gameState.isTerminalState())
-//     {
+class AgentTrainer
+{
+private:
 
-//     }
-//     std::vector<move> legalMoves = gameState.generateMoves();
-//     gameState gameResult = gameState.getEvaluation();
-//     for (std::vector<move>::iterator moveIterator = legalMoves.begin(); moveIterator != legalMoves.end(); moveIterator++)
-//     {
-//         Ultimate3TState successor = gameState.generateSuccessorState(*moveIterator);
-//         // search for successor in table
-//         //    gameResult = table[successor].getEvaluation();
-//         // else 
-//         {
-//             gameResult = study(successor);
-//             if (gameResult == evaluation::oWin &&
-//                 gameState.getActivePlayer() == toPlay::o &&
-//                 )
-//                 {
+    std::set<std::bitset<ENCODINGSIZE>, EncodingCompare> transpositionTable_;
 
-//                 }
-//         }
-//     }
-// }
+    Ultimate3TState startState_;
+
+    void init();
+
+public:
+    AgentTrainer();
+    AgentTrainer(const Ultimate3TState startState);
+
+    evaluationValue minimax(Ultimate3TState& state, int depth = 0);
+
+    void writeToFile();
+};
+
