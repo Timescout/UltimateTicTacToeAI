4630a6fef65e182a2d422398a6253f68c364f664
Andrew Bergman <timepolicescout@gmail.com>
2023-10-13 15:36:39 -0700

    ran a test with sets of bitsets

--- code/main.cpp
+++ code/main.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 #include <bitset>
 #include <fstream>
+#include <set>
 #include "State.h"
+#include "Agent.h"
 
 int main()
 {
     std::cout << "hello World\n";
 
-    std::bitset<196> encoding(1);
-    encoding <<= 2;
+    // std::bitset<196> encoding(1);
+    // encoding <<= 2;
 
 
 
 
-    std::fstream file("brain.txt");
-    if(!file.is_open()) { std::cerr << "file failed to open\n"; throw; }
-    file << encoding;
-    file.close();
+    // std::fstream file("brain.txt");
+    // if(!file.is_open()) { std::cerr << "file failed to open\n"; throw; }
+    // file << encoding;
+    // file.close();
+
+
+    std::bitset<ENCODINGSIZE> seven(7);
+    std::bitset<ENCODINGSIZE> eight(8);
+
+    std::set<std::bitset<ENCODINGSIZE>, EncodingCompare> mySet;
+
+    mySet.insert(seven);
+    mySet.insert(eight);
+
+    std::cout << mySet.size() << "\n";
+
     return 0;
 }
