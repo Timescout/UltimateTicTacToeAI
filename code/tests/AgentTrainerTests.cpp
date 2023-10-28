05045e2c01461145307e51404d6797519598d4f4
Andrew Bergman <timepolicescout@gmail.com>
2023-10-28 03:16:26 -0700

    added code for testing transposition table, used better string output checking.

--- code/tests/AgentTrainerTests.cpp
+++ code/tests/AgentTrainerTests.cpp
@@ -21,6 +21,26 @@ namespace AgentTrainerTestFunctions
         }
         return state;
     }
+
+    // Creates a state where the players play a "normal" game of tic tac toe
+    Ultimate3TState createSmallGame()
+    {
+        Ultimate3TState state;
+
+        for (int i = 0; i < 9; i++)
+        {
+            state.setSpacePlayed(0, i, draw);
+            state.setSpacePlayed(1, i, draw);
+            state.setSpacePlayed(2, i, x);
+            state.setSpacePlayed(3, i, draw);
+            state.setSpacePlayed(4, i, draw);
+            state.setSpacePlayed(5, i, x);
+            state.setSpacePlayed(6, i, o);
+            state.setSpacePlayed(7, i, o);
+        }
+
+        return state;
+    }
 }
 using namespace AgentTrainerTestFunctions;
 
@@ -33,9 +53,26 @@ TEST(AgentTrainerTests, Minimax_TerminalState_DoesNotExpandMoreStates)
 
     trainer.minimax(state);
     trainer.writeToOutput();
-    std::string output;
-    outputStream >> output;
+    std::string output = outputStream.str();
     
     EXPECT_EQ(outputStream.str().size(), ENCODINGSIZE+1); // test that there is only one entry in the output. size should be encodingsize + 1 since there should be a terminating newline character at the end of the encoding.
-    EXPECT_EQ(state.toBinary().to_string(), output);
+    EXPECT_EQ(state.toBinary().to_string() + "\n", output);
+}
+
+TEST(AgentTrainerTests, MinimaxTranspositionTable_SimpleLineTree_ExpandsCorrectNumberOfStates)
+{
+    std::stringstream outputStream;
+    AgentTrainer trainer(outputStream);
+    Ultimate3TState state = createSmallGame();
+    for (int i = 3; i < 9; i++)
+    {
+        state.setSpacePlayed(8, i, draw);
+    }
+
+    trainer.minimax(state);
+    trainer.writeToOutput();
+    std::string output = outputStream.str();
+
+    EXPECT_EQ(trainer.getStatesExpanded(), 10);
+    EXPECT_EQ(output.size(), (ENCODINGSIZE+1) * 13);
 }
