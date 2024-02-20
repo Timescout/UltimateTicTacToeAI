635e79eb0568637ce83ce0de05732cd452e41a5b
Timescout <timepolicescout@gmail.com>
2024-02-19 21:20:43 -0800

    added some documentation and generics trickery

--- source/GameState.cs
+++ source/GameState.cs
@@ -6,9 +6,12 @@ namespace GameLogic
     /// Abstract class for a two player game with perfect information.
     /// </summary>
     /// <typeparam name="MoveType">The move class associated with this game.</typeparam>
-    public abstract class GameStateBase <MoveType>
+    /// <typeparam name="GameState">Because we want generateSuccessor to return an instance of the class which inherits from GameStateBase, GameStateBase must take that type as an argument. Feels very tricky and confusing.</typeparam>
+    public abstract class GameStateBase <MoveType, GameState>
     {
-
+        /// <summary>
+        /// Must Return an array of legal moves in this state. If this list is empty then the state must be Terminal and isTerminal() must return true.
+        /// </summary>
         public abstract MoveType[] generateMoves();
 
         /// <summary>
@@ -16,10 +19,20 @@ namespace GameLogic
         /// </summary>
         public abstract bool isTerminal();
 
-        public abstract GameStateBase<MoveType> generateSuccessor(MoveType action);
+        /// <summary>
+        /// Must return a state where the given action was takken in this state. The return type is the child class of this base class. To get an array of possible actions, use generateMoves(). If this list is empty, then the state is terminal and isTerminal will be true.
+        /// </summary>
+        /// <param name="action">The legal action takken in this state.</param>
+        public abstract GameState generateSuccessor(MoveType action);
 
+        /// <summary>
+        /// Must Return the utility of this state where high values are good for max nodes and low values are good for min nodes.
+        /// </summary>
         public abstract int utility();
 
+        /// <summary>
+        /// Must return whether this state is a max node or not.
+        /// </summary>
         public abstract bool isMaxNode();
     }
 }
