e28f228b4d62b873dc21e0f114d77f5def3bb7f1
Timescout <timepolicescout@gmail.com>
2024-02-24 16:28:35 -0800

    added factory class and some tests.

--- tests/UltimateTicTacToeStateTests.cs
+++ tests/UltimateTicTacToeStateTests.cs
@@ -6,6 +6,56 @@ using GameLogic;
 
 namespace UltimateTicTacToeStateTests
 {
+    // inherits from UltimateTicTacToeState so it can access protected variables.
+    public class UltimateTicTacToeStateFactory : UltimateTicTacToeState
+    {
+        // Creates a state where the top left bottom right diagonal and all horizontal win conditions must be right to be terminal
+        public static UltimateTicTacToeState createTerminalStateXWins()
+        {
+            UltimateTicTacToeState state = new UltimateTicTacToeState();
+            state.board[0][0] = 'X';
+            state.board[0][1] = 'X';
+            state.board[0][2] = 'X';
+            
+            state.board[4][3] = 'X';
+            state.board[4][4] = 'X';
+            state.board[4][5] = 'X';
+
+            state.board[8][6] = 'X';
+            state.board[8][7] = 'X';
+            state.board[8][8] = 'X';
+
+            for (int i = 0; i < 9; i++)
+            {
+                state.boardStatus[i] = TicTacToeEvaluation(state.board[i]);
+            }
+            return state;
+        }
+
+        // Creates a state where the top right to bottom left diagonal and all vertical win conditiona must be right to be terminal
+        public static UltimateTicTacToeState createTerminalStateOWins()
+        {
+            UltimateTicTacToeState state = new UltimateTicTacToeState();
+            state.board[2][0] = 'O';
+            state.board[2][3] = 'O';
+            state.board[2][6] = 'O';
+            
+            state.board[4][1] = 'O';
+            state.board[4][4] = 'O';
+            state.board[4][7] = 'O';
+
+            state.board[6][2] = 'O';
+            state.board[6][5] = 'O';
+            state.board[6][8] = 'O';
+            for (int i = 0; i < 9; i++)
+            {
+                state.boardStatus[i] = TicTacToeEvaluation(state.board[i]);
+            }
+            return state;
+        }
+    }
+
+
     [TestFixture]
     public class UltimateTicTacToeStateTester
     {
@@ -106,6 +156,52 @@ namespace UltimateTicTacToeStateTests
             moves = state.generateMoves();
 
             Assert.That(moves.Count, Is.EqualTo(81), "Starting State should have 81 legal moves");
+        }
+
+        [Test]
+        public void isTerminal_TerminalXWins_returnsTrue()
+        {
+            UltimateTicTacToeState state = UltimateTicTacToeStateFactory.createTerminalStateXWins();
+
+            bool result = state.isTerminal();
+
+            Assert.That(result, Is.True,"Terminal State should return true from isTerminal");
+        }
+
+        [Test]
+        public void isTerminal_TerminalOWins_returnsTrue()
+        {
+            UltimateTicTacToeState state = UltimateTicTacToeStateFactory.createTerminalStateOWins();
+
+            bool result = state.isTerminal();
+
+            Assert.That(result, Is.True,"Terminal State should return true from isTerminal");
+        }
+
+        [Test]
+        public void TicTacToeEvaluation_oneX_returnsN()
+        {
+            List<char> list = new List<char>();
+            for (int i = 0; i < 9; i++)
+            {
+                list.Add('N');
+            }
+            list[0] = 'X';
+
+            char result = UltimateTicTacToeState.TicTacToeEvaluation(list);
+
+            Assert.That(result, Is.EqualTo('N'), "List With only one X should return N from TicTacToeEvaluation");
+        }
+
+        [Test]
+        public void evaluation_DifferentDepths_prioritizesLowerDepth()
+        {
+            int highValue = UltimateTicTacToeState.evaluation('X', 1);
+            int lowValue = UltimateTicTacToeState.evaluation('X', 2);
+
+            bool result = highValue > lowValue;
+
+            Assert.That(result, Is.True, "Wins with lower depth should be prioritized");
         }
 
     }
